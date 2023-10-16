f16bbfabcb70c1607865ac195eac35228474edbd
Eric Nordgren <eric.nordgren@bybrick.se>
2023-10-16 19:24:14 +0200

    Creation of empty square net works

---
+++ Rutnät/Presentation/Controllers/SquareNetController.cs
@@ -0,0 +1,65 @@
+﻿using IdentityServer4.Extensions;
+using Infrastructure.Entities;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SquareNetController : ControllerBase
+    {
+        private readonly ISquareNetRepository _squareNetRepository;
+        public SquareNetController(ISquareNetRepository squareNetRepository)
+        {
+            _squareNetRepository = squareNetRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SquareNet>> GetSquareNetAsync(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SquareNet>> AddNewSquareNetAsync()
+        {
+
+            try
+            {
+                var userId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return BadRequest("No user id");
+                }
+
+                if (Guid.TryParse(userId, out var id))
+                {
+                    var squares = Enumerable.Range(1, 25).Select(_ => new Square()).ToList();
+
+                    var squareNet = new SquareNet
+                    {
+                        ApplicationUserId = Guid.Parse(userId),
+                        Squares = squares,
+                    };
+
+                    await _squareNetRepository.CreateSquareNetAsync(squareNet);
+
+                    return CreatedAtAction(nameof(GetSquareNetAsync), new { id = squareNet.Id }, squareNet);
+                }
+                else
+                {
+                    return BadRequest("Invalid user id format");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+    }
+}
--- Rutnät/client-app/src/components/Home/Home.tsx
+++ Rutnät/client-app/src/components/Home/Home.tsx
@@ -1,18 +1,33 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { PageContainer } from '../layout/PageContainer'
 import { SquareNetContainer } from '../SquareNetContainer'
 import { Button } from '../layout/Button/Button'
 import { PageContentContainer } from '../layout/PageContentContainer'
 import { SquareNetList } from '../SquareNetList'
 import { SaveSquareNetMenu } from '../SaveSquareNetMenu'
+import { FetchStatus, HttpMethod } from '../../enums'
+import { apiFetch } from '../../integration'
 
 type Props = {}
 
 export const Home: FC<Props> = () => {
+  const [uploadStatus, setUploadStatus] = useState<FetchStatus>(FetchStatus.Idle);
+
+  const handleCreateNewSquareNet = () => {
+    setUploadStatus(FetchStatus.Loading);
+    apiFetch(`https://localhost:7162/api/SquareNet`, undefined, HttpMethod.POST, true, 'application/json', true)
+    .then(() => {
+        setUploadStatus(FetchStatus.Success);
+    }).catch((err) => {
+        console.log(err)
+        setUploadStatus(FetchStatus.Error);
+    });
+}
+
   return (
     <PageContainer>
         <PageContentContainer>
-            <Button text="Create New" style={{marginBottom: 10, alignSelf: 'flex-end'}}/>
+            <Button text="Create New" style={{marginBottom: 10, alignSelf: 'flex-end'}} onClick={handleCreateNewSquareNet}/>
             <SquareNetContainer />
             <SaveSquareNetMenu />
             <SquareNetList squareNets={[1,2,3,4,5,6,7,8]} />
